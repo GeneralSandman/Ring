8c13a00780b778cd54e1cc8aa46f2481de8ce0f4
zhenhuli <zhenhuli@tencent.com>
2022-01-19 11:23:44 +0800

    update:

--- ring.h
+++ ring.h
@@ -1,6 +1,28 @@
 #ifndef RING_INCLUDE_H
 #define RING_INCLUDE_H
 
+typedef enum {
+
+} StatementType;
+
+typedef struct {
+    StatementType type;
+    int line_number;
+} Statement;
+
+typedef enum {
+    EXPRESSION_TYPE_UNKNOW = 1,
+
+    EXPRESSION_TYPE_Variable_Definition,
+    EXPRESSION_TYPE_Function_Call,
+} ExpressionType;
+
+typedef struct {
+    ExpressionType type;
+    int line_number;
+
+} Expression;
+
 void init_string_literal_buffer();
 void reset_string_literal_buffer();
 void string_literal_add_char(char ch);
