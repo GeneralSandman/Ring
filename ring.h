a9adad26ed43c6a547da95b750a66c2cdf1bd75d
zhenhuli <zhenhuli@tencent.com>
2022-02-04 12:05:48 +0800

    update:

--- ring.h
+++ ring.h
@@ -1,7 +1,7 @@
 #ifndef RING_INCLUDE_H
 #define RING_INCLUDE_H
 
-#define RING_VERSION "ring-v0.0.6-beat"
+#define RING_VERSION "ring-v0.0.8-beat"
 
 typedef struct Ring_Interpreter_Tag Ring_Interpreter;
 
