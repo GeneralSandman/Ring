fd62229314a5f2a3cf272340cb9e2e4ff73e0d68
zhenhuli <zhenhuli@tencent.com>
2023-04-06 15:08:02 +0800

    update:

--- ring.wiki/publish.sh
+++ ring.wiki/publish.sh
@@ -6,7 +6,8 @@
 # 通过 cos-bucket website 功能部署
 
 
-# coscmd config -a secretID  -s secretKEY -b ring-1252697766 -r ap-beijing
+cd ring.wiki
+# coscmd config -a AKIDVv9JH381KRM5dyC60rWzHNGzVpxso9Wk  -s Jn6D8sQnxvhHeqpcDcswozznPs9OWYN0 -b ring-1252697766 -r ap-beijing
 coscmd upload -r ./ / 
 
 #--ignore "./.git/*,./leetcode/leetcode-crawler/*"%
