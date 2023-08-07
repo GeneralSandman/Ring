b614cc587e7c2630e5170eef69eed3dbe0bd7524
GeneralSandman <generalsandman@163.com>
2023-08-07 14:54:51 +0800

    📃 docs: update 2023路线图

--- ring.wiki/publish.sh
+++ ring.wiki/publish.sh
@@ -6,8 +6,8 @@
 # 通过 cos-bucket website 功能部署
 
 
-cd ring.wiki
-coscmd config -a x  -s x -b ring-1252697766 -r ap-beijing
+# cd ring.wiki
+# coscmd config -a x  -s x -b ring-1252697766 -r ap-beijing
 coscmd upload -r ./ / 
 
 echo "open https://ring-1252697766.cos-website.ap-beijing.myqcloud.com\n"
