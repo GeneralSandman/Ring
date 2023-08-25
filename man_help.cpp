37576241bc50103940f4c061e61074353c77b2fd
GeneralSandman <generalsandman@163.com>
2023-08-25 10:13:21 +0800

    🐞update(ring man help page):
    add ring man help page.
    usage: ring man <keyword>

--- man_help.cpp
+++ man_help.cpp
@@ -2,29 +2,117 @@
 #include <stdio.h>
 #include <string>
 
-std::string a =
-    "\033[0;31m*General Ring-Language Gramnmer Manual*\033[0m\n"
+std::string variable_definite =
+    "\033[0;31m General Ring-Language Gramnmer Manual \033[0m\n"
     "\n"
 
     "\033[0;33m+Name:\033[0m\n"
-    "    Ring Quick Start HelloWorld\n"
+    "    variable\n"
     "\n"
 
-    "\033[0;33m+DESCRIPTION:\033[0m\n"
-    "    This program is a simple content of Ring-Language.\n"
+    "\033[0;33m+Description:\033[0m\n"
+    "    How to definite a variable.\n"
     "\n"
 
-    "\033[0;33m+USAGE:\033[0m\n"
+    "\033[0;33m+Gramnmer/Usage:\033[0m\n"
+    "    var bool/int/double/string <identifier> [ =  initializer];\n"
+    "\n"
+
+    "\033[0;33m+Example:\033[0m\n"
     "    var bool bool_value;\n"
     "    var int int_value;\n"
-    "    var float float_value;\n"
+    "    var double double_value;\n"
     "    var string string_value;\n"
     "\n"
 
     "\033[0;33m+Notice:\033[0m\n"
     "\n";
 
+std::string function_definite =
+    "\033[0;31m General Ring-Language Gramnmer Manual \033[0m\n"
+    "\n"
+
+    "\033[0;33m+Name:\033[0m\n"
+    "    function\n"
+    "\n"
+
+    "\033[0;33m+Description:\033[0m\n"
+    "    How to definite a function.\n"
+    "\n"
+
+    "\033[0;33m+Gramnmer/Usage:\033[0m\n"
+    "   function <identifier> (parameter_list) -> (return_value_list) {\n"
+    "       code_block;\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Example:\033[0m\n"
+    "   function doit1() {\n"
+    "       fmt::println_string(\"I'm function doit1, go go go!\");\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Notice:\033[0m\n"
+    "\n";
+
+std::string global_variable_definite =
+    "\033[0;31m General Ring-Language Gramnmer Manual \033[0m\n"
+    "\n"
+
+    "\033[0;33m+Name:\033[0m\n"
+    "    global variable\n"
+    "\n"
+
+    "\033[0;33m+Description:\033[0m\n"
+    "    How to definite global variable.\n"
+    "\n"
+
+    "\033[0;33m+Gramnmer/Usage:\033[0m\n"
+    "   global {\n"
+    "       var bool/int/double/string <identifier>;\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Example:\033[0m\n"
+    "   global {\n"
+    "       var bool bool_value;\n"
+    "       var int int_value;\n"
+    "       var double double_value;\n"
+    "       var string string_value;\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Notice:\033[0m\n"
+    "\n";
+
+std::string import =
+    "\033[0;31m General Ring-Language Gramnmer Manual \033[0m\n"
+    "\n"
+
+    "\033[0;33m+Name:\033[0m\n"
+    "    import package\n"
+    "\n"
+
+    "\033[0;33m+Description:\033[0m\n"
+    "    How to import package in head of Ring source code file.\n"
+    "\n"
+
+    "\033[0;33m+Gramnmer/Usage:\033[0m\n"
+    "   import {\n"
+    "       <identifier>;\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Example:\033[0m\n"
+    "   import {\n"
+    "       fmt;\n"
+    "       debug;\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Notice:\033[0m\n"
+    "\n";
 
 void ring_give_man_help(char* keyword) {
-    printf("%s\n", a.c_str());
+    printf("%s", import.c_str());
 }
