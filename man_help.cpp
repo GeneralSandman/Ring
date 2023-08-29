5f6c4633d30b5ad7ed05679861f54e1504adda98
GeneralSandman <generalsandman@163.com>
2023-08-29 09:49:40 +0800

    ✨ feat(add ring man keyword): add ring man keyword: class

--- man_help.cpp
+++ man_help.cpp
@@ -9,6 +9,7 @@ std::string help_list =
     "    \033[0;31m ring man func \033[0m        :function definite\n"
     "    \033[0;31m ring man global_var \033[0m  :global variable definite\n"
     "    \033[0;31m ring man import \033[0m      :import package\n"
+    "    \033[0;31m ring man class  \033[0m      :class definite\n"
     "    \033[0;31m ring man --list \033[0m      :list all man keyword\n"
     "\n";
 
@@ -123,6 +124,42 @@ std::string import =
     "\033[0;33m+Notice:\033[0m\n"
     "\n";
 
+std::string class_definite =
+    "\033[0;31m General Ring-Language Gramnmer Manual \033[0m\n"
+    "\n"
+
+    "\033[0;33m+Name:\033[0m\n"
+    "    class\n"
+    "\n"
+
+    "\033[0;33m+Description:\033[0m\n"
+    "    How to definite a class type.\n"
+    "\n"
+
+    "\033[0;33m+Gramnmer/Usage:\033[0m\n"
+    "   typedef class <identifier> {\n"
+    "       <field_list>;\n"
+    "       <method_list>;\n"
+    "   }\n"
+    "\n"
+
+    "\033[0;33m+Example:\033[0m\n"
+    "   typedef class Job {\n"
+    "       field bool    Running;\n"
+    "       field int     JobID;\n"
+    "       field double  Score;\n"
+    "   \n"
+    "       @constructor\n"
+    "       method Job(var bool _Running, var int _JobID, var double _Score);\n"
+    "   \n"
+    "       method PrintInfo();\n"
+    "   }\n"
+    "\n"
+
+
+    "\033[0;33m+Notice:\033[0m\n"
+    "\n";
+
 void ring_give_man_help(char* keyword) {
     std::string res = help_list;
     if (0 == strcmp(keyword, "var")) {
@@ -133,6 +170,8 @@ void ring_give_man_help(char* keyword) {
         res = global_variable_definite;
     } else if (0 == strcmp(keyword, "import")) {
         res = import;
+    } else if (0 == strcmp(keyword, "class")) {
+        res = class_definite;
     } else if (0 == strcmp(keyword, "--list")) {
         res = help_list;
     }
