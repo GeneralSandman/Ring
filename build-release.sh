6777c4c4cb4af16eeba76e89434581e035e6ec75
GeneralSandman <generalsandman@163.com>
2023-12-16 22:16:35 +0800

    🐞update(Makefile&build-release):
    1. open -O2 in compiler.
    2. change build-release.sh.

--- build-release.sh
+++ build-release.sh
@@ -2,18 +2,26 @@
 
 # start ubuntu docker
 
-docker run -it -v /Users/bytedance/Desktop/Ring:/home/Ring --name test-ring-ubuntu ubuntu bash
+docker run --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
+    -it -v /Users/bytedance/Desktop/Ring:/home/Ring --name test-ring-ubuntu ubuntu bash
 
 
-apt-get update
-
+apt-get update && \
 apt-get -y install make && \
 apt-get -y install flex && \
 apt-get -y install bison && \
 apt-get -y install g++ && \
-apt-get -y install gdb
+apt-get -y install gdb && \
+apt-get -y install valgrind && \
+cd /home/Ring && \
+make clean && make && make install && make testall
 
-make clean && make
+
+valgrind --leak-check=full \
+    --show-leak-kinds=all  \
+    --track-origins=yes -s \
+    ./bin/ring run ./test/024-array-class/for-access-002.ring 2>valgrind.result
+
 
 
 # start centos docker
@@ -21,22 +29,18 @@ make clean && make
 docker run -it -v /Users/bytedance/Desktop/Ring:/home/Ring --name test-ring-centos centos bash
 
 
-cd /etc/yum.repos.d/
-
-sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
-sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+cd /etc/yum.repos.d/ && \
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-* && \
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-* && \
 yum update -y
 
 
 
-yum install make
-
-yum install epel-release
-
-yum debuginfo-install libgcc-8.5.0-4.el8_5.x86_64 libstdc++-8.5.0-4.el8_5.x86_64
-
-yum install clang
-
-yum install flex
-
-yum install bison
+yum install make && \
+yum install flex && \
+yum install bison && \
+yum install clang && \
+yum install gdb && \
+yum install valgrind && \
+cd /home/Ring && \
+make clean && make && make install && make testall
