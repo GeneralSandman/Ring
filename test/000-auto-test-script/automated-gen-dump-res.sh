c17994523d2f03d0f8e1929356d95575cb60c82c
GeneralSandman <generalsandman@163.com>
2024-11-24 17:56:27 +0800

    🧪 test(update test result of `ring dump`):

---
+++ test/000-auto-test-script/automated-gen-dump-res.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+./bin/ring -O0 dump ./test/004-derive-function/function-000.ring       > ./test/004-derive-function/function-000.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-001.ring       > ./test/004-derive-function/function-001.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-002.ring       > ./test/004-derive-function/function-002.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-003.ring       > ./test/004-derive-function/function-003.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-004.ring       > ./test/004-derive-function/function-004.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-005.ring       > ./test/004-derive-function/function-005.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-006.ring       > ./test/004-derive-function/function-006.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-009.ring       > ./test/004-derive-function/function-009.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-020.ring       > ./test/004-derive-function/function-020.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-021.ring       > ./test/004-derive-function/function-021.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-010.ring       > ./test/004-derive-function/function-010.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-011.ring       > ./test/004-derive-function/function-011.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-012.ring       > ./test/004-derive-function/function-012.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-013.ring       > ./test/004-derive-function/function-013.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-014.ring       > ./test/004-derive-function/function-014.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-015.ring       > ./test/004-derive-function/function-015.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-016.ring       > ./test/004-derive-function/function-016.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-017.ring       > ./test/004-derive-function/function-017.ring.dump                                                 
+./bin/ring -O0 dump ./test/001-basic-type/string-000.ring              > ./test/001-basic-type/string-000.ring.dump                                                 
+./bin/ring -O0 dump ./test/004-derive-function/function-019.ring       > ./test/004-derive-function/function-019.ring.dump                                                 
+./bin/ring -O0 dump ./test/001-basic-type/string-001.ring              > ./test/001-basic-type/string-001.ring.dump                                                 
+./bin/ring -O0 dump ./test/001-basic-type/string-002.ring              > ./test/001-basic-type/string-002.ring.dump                                                 
+./bin/ring -O0 dump ./test/001-basic-type/string-003.ring             > ./test/001-basic-type/string-003.ring.dump
